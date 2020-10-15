4c6cf36a396782f0dbae9f8704125353bc2b8ca3
UMSI Web Design <collemc@umich.edu>
2020-10-15 14:47:41 -0400

    Add files via upload

---
+++ js/weather.js
@@ -0,0 +1,40 @@
+function gettingJSON(){
+    //Display the forecast
+    // Your code here.
+
+    //Set default location if one isn't provided
+    let location;
+
+    // Your code here.
+    console.log("Location is : " + location);
+
+    //set default temperature format if one isn't provided
+    let fomat;
+
+    // Your code here.
+    console.log("Format is " + format);
+
+    //set the query  
+    let query;
+    // Your code here.  
+    
+    console.log("Query is :" + query);
+
+    //Create and set variables for each of the elements you
+    //need to update, location, highs and lows, 
+    //the image, etc.
+
+    let loc;
+    let temp;
+    let tempImg;
+    // Your code here.
+
+
+    $.getJSON(query,function(json){
+        //Use returned json to update the values of the three 
+        //elements in HTML.  
+        //I would print the JSON to the console
+        // Your code here.
+
+    });
+}
